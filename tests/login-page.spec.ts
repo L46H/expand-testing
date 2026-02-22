3958824fb066247112ec7ff495e65e7d553bfb42
Hubert Kieruczenko <hub3rt65@gmail.com>
2026-02-22 15:32:16 +0100

    test: login with invalid username

--- tests/login-page.spec.ts
+++ tests/login-page.spec.ts
@@ -1,20 +1,39 @@
 import { test, expect } from '../fixtures/adblock.fixture';
 
+const credentials = {
+  username: 'practice',
+  password: 'SuperSecretPassword!',
+  invalidUsername: 'invPractice'
+};
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/login');
+});
+
 test('login and logout successfully', async ({ page }) => {
-    await page.goto('/login');
+  const usernameInput = page.getByLabel('Username');
+  const passwordInput = page.getByLabel('Password');
+  const loginButton = page.getByRole('button', { name: 'Login' });
+  const logoutButton = page.getByRole('link', { name: 'Logout' });
 
-    const usernameInput = page.getByLabel('Username');
+  await usernameInput.fill(credentials.username);
+  await passwordInput.fill(credentials.password);
+  await loginButton.click();
+  await expect(page.getByText('You logged into a secure area!')).toBeVisible();
+
+  await logoutButton.click();
+  await expect(
+    page.getByText('You logged out of the secure area!')
+  ).toBeVisible();
+});
+
+test('login with invalid username', async ({ page }) => {
     const passwordInput = page.getByLabel('Password');
+    const usernameInput = page.getByLabel('Username');
     const loginButton = page.getByRole('button', { name: 'Login' });
-    const logoutButton = page.getByRole('link', { name: 'Logout' });
 
-    const credentials = { username: 'practice', password: 'SuperSecretPassword!' };
-
-    await usernameInput.fill(credentials.username);
-    await passwordInput.fill(credentials.password);
-    await loginButton.click();
-    await expect(page.getByText('You logged into a secure area!')).toBeVisible();
-
-    await logoutButton.click();
-    await expect(page.getByText('You logged out of the secure area!')).toBeVisible();
+  await usernameInput.fill(credentials.invalidUsername);
+  await passwordInput.fill(credentials.password);
+  await loginButton.click();
+  await expect(page.getByText('Your username is invalid!')).toBeVisible();
 });
