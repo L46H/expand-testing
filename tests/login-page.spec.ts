b53cd936029401b8c4b682f4f3d0e74abda2897a
Hubert Kieruczenko <hub3rt65@gmail.com>
2026-02-22 16:27:03 +0100

    test: login with invalid password

--- tests/login-page.spec.ts
+++ tests/login-page.spec.ts
@@ -3,7 +3,8 @@ import { test, expect } from '../fixtures/adblock.fixture';
 const credentials = {
   username: 'practice',
   password: 'SuperSecretPassword!',
-  invalidUsername: 'invPractice'
+  invalidUsername: 'invPractice',
+  invalidPassword: 'invPass'
 };
 
 test.beforeEach(async ({ page }) => {
@@ -28,12 +29,25 @@ test('login and logout successfully', async ({ page }) => {
 });
 
 test('login with invalid username', async ({ page }) => {
-    const passwordInput = page.getByLabel('Password');
-    const usernameInput = page.getByLabel('Username');
-    const loginButton = page.getByRole('button', { name: 'Login' });
+  const usernameInput = page.getByLabel('Username');
+  const passwordInput = page.getByLabel('Password');
+  const loginButton = page.getByRole('button', { name: 'Login' });
 
   await usernameInput.fill(credentials.invalidUsername);
   await passwordInput.fill(credentials.password);
+
   await loginButton.click();
   await expect(page.getByText('Your username is invalid!')).toBeVisible();
+});
+
+test('login with invalid password', async ({ page }) => {
+  const usernameInput = page.getByLabel('Username');
+  const passwordInput = page.getByLabel('Password');
+  const loginButton = page.getByRole('button', { name: 'Login' });
+
+  await usernameInput.fill(credentials.username);
+  await passwordInput.fill(credentials.invalidPassword);
+
+  await loginButton.click();
+  await expect(page.getByText('Your password is invalid!')).toBeVisible();
 });
