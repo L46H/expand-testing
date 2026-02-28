b41f9bd5d074a9067f9e57203ad5c8fdddf61f76
Hubert Kieruczenko <hub3rt65@gmail.com>
2026-02-28 13:42:15 +0100

    refactor(login): add LoginPage POM and update tests

--- tests/login-page.spec.ts
+++ tests/login-page.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '../fixtures/adblock.fixture';
+import { LoginPage } from '../pages/login.page';
 
 const credentials = {
   username: 'practice',
@@ -7,47 +8,29 @@ const credentials = {
   invalidPassword: 'invPass'
 };
 
+let loginPage: LoginPage;
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/login');
+  loginPage = new LoginPage(page);
 });
 
 test('login and logout successfully', async ({ page }) => {
-  const usernameInput = page.getByLabel('Username');
-  const passwordInput = page.getByLabel('Password');
-  const loginButton = page.getByRole('button', { name: 'Login' });
-  const logoutButton = page.getByRole('link', { name: 'Logout' });
-
-  await usernameInput.fill(credentials.username);
-  await passwordInput.fill(credentials.password);
-  await loginButton.click();
+  await loginPage.login(credentials.username, credentials.password);
   await expect(page.getByText('You logged into a secure area!')).toBeVisible();
 
-  await logoutButton.click();
+  await loginPage.logout();
   await expect(
     page.getByText('You logged out of the secure area!')
   ).toBeVisible();
 });
 
 test('login with invalid username', async ({ page }) => {
-  const usernameInput = page.getByLabel('Username');
-  const passwordInput = page.getByLabel('Password');
-  const loginButton = page.getByRole('button', { name: 'Login' });
-
-  await usernameInput.fill(credentials.invalidUsername);
-  await passwordInput.fill(credentials.password);
-
-  await loginButton.click();
+  await loginPage.login(credentials.invalidUsername, credentials.password);
   await expect(page.getByText('Your username is invalid!')).toBeVisible();
 });
 
 test('login with invalid password', async ({ page }) => {
-  const usernameInput = page.getByLabel('Username');
-  const passwordInput = page.getByLabel('Password');
-  const loginButton = page.getByRole('button', { name: 'Login' });
-
-  await usernameInput.fill(credentials.username);
-  await passwordInput.fill(credentials.invalidPassword);
-
-  await loginButton.click();
+  await loginPage.login(credentials.username, credentials.invalidPassword);
   await expect(page.getByText('Your password is invalid!')).toBeVisible();
 });
