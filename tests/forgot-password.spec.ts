ed5fab132d6994a393b9c4c9b6fc8d53755b070e
Hubert Kieruczenko <hub3rt65@gmail.com>
2026-05-02 11:03:31 +0200

    test(forgot-password): add success alert assertion

--- tests/forgot-password.spec.ts
+++ tests/forgot-password.spec.ts
@@ -7,4 +7,8 @@ test.beforeEach(async ({ page }) => {
 test('successful password reset', async ({ page }) => {
   await page.getByLabel('E-mail').fill('test@example.com');
   await page.getByRole('button', { name: 'Retrieve password' }).click();
+
+  await expect(page.getByRole('alert')).toContainText(
+    'An e-mail has been sent to you which explains how to reset your password.'
+  );
 });
