b7d4c51a0fe811602417e0a5bf8bd1c40f159ad5
Hubert Kieruczenko <hub3rt65@gmail.com>
2026-06-09 08:14:02 +0200

    test(api): add health check test

---
+++ tests/api/health.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('successful health check', async ({ request }) => {
+  const response = await request.get(
+    'https://practice.expandtesting.com/notes/api/health-check'
+  );
+  expect(response.status()).toBe(200);
+  
+  const jsonData = await response.json();  
+  expect(jsonData.success).toBe(true);
+});
