fe70d3dce8b28b231bbac31d521304400c16b96d
Hubert Kieruczenko <hub3rt65@gmail.com>
2026-06-10 08:57:07 +0200

    refactor(config): separate UI and API test projects

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,10 +51,22 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    },
+      {
+    name: 'ui',
+    testDir: './tests/ui',
+    use: {
+      ...devices['Desktop Chrome'],
+      baseURL
+    }
+  },
+  {
+    name: 'api',
+    testDir: './tests/api',
+  }
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] }
+    // },
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
