f9a56571c01328377a6e070a395c69e6b5b6a6da
Hubert Kieruczenko <hub3rt65@gmail.com>
2026-07-05 21:20:17 +0200

    test(api): add login with invalid credentials test

--- tests/api/login.spec.ts
+++ tests/api/login.spec.ts
@@ -28,3 +28,18 @@ test('login with empty body', async ({ request }) => {
   expect(response.status()).toBe(400);
   expect(jsonData.success).toBe(false);
 });
+
+test('login with invalid credentials', async ({ request }) => {
+  const { email, password } = loginData.invalidLogin;
+
+  const response = await request.post(endpoints.login, {
+    data: {
+      email,
+      password
+    }
+  });
+  const jsonData = await response.json();
+
+  expect(response.status()).toBe(401);
+  expect(jsonData.success).toBe(false);
+});
